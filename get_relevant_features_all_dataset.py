fd7d283b39eb0d83c93d326d41fbac9fa6fad5fb
Wylken S. Machado <wylken.ufal@gmail.com>
2019-12-05 17:46:15 -0300

    fix script

--- get_relevant_features_all_dataset.py
+++ get_relevant_features_all_dataset.py
@@ -2,7 +2,7 @@
 import sys
 from utils.project import slash
 
-exec(open("scripts{}arcma{}arcma_get_all_relevant_features_each_person.py".format(slash, slash)))
-exec(open("scripts{}hmp{}hmp_get_all_relevant_features_each_person.py".format(slash, slash)))
-exec(open("scripts{}umafall{}umafall_get_all_relevant_features_each_person.py".format(slash, slash)))
+exec(open("scripts{}arcma{}arcma_get_all_relevant_features_each_person.py".format(slash, slash)).read())
+exec(open("scripts{}hmp{}hmp_get_all_relevant_features_each_person.py".format(slash, slash)).read())
+exec(open("scripts{}umafall{}umafall_get_all_relevant_features_each_person.py".format(slash, slash)).read())
 
