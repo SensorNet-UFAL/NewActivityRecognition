932f651ec539182e8a8091e8e8b1c6e9c8b88cc1
Wylken S. Machado <wylken.ufal@gmail.com>
2019-12-15 15:09:57 -0300

    update best window umafall and arcma

--- scripts/hmp/hmp_get_all_relevant_features_each_person.py
+++ scripts/hmp/hmp_get_all_relevant_features_each_person.py
@@ -3,8 +3,6 @@
 from utils.debug import Debug
 from models.hmp_model import HMP_Model
 from tsfresh import extract_relevant_features
-import pandas as pd
-import numpy as np
 from pre_processing.processing_db_files import Processing_DB_Files
 from utils.project import Project, slash
 from scripts.save_workspace import save
@@ -15,33 +13,21 @@ hmp = HMP_Model()
 processing = Processing_DB_Files()
 project = Project()
 s = save()
-data_list_people = hmp.load_training_data_from_list_people(16, ["f1", "m1", "m2", "f2", "m3", "f3", "m4", "m5", "m6", "m7", "f4", "m8", "m9", "f5", "m10", "m11"], remove_outliers=0.05) # Janela Fixa
-#data_list_people = hmp.load_training_data_from_list_people(80, ["f1", "m1", "m2", "f2", "m3", "f3", "m4", "m5", "m6", "m7", "f4", "m8", "m9", "f5", "m10", "m11"], remove_outliers=0.05) # Janela encontrada
+#window = 16 # Janela Fixa
+window = 50 # Melhor Janela
+persons = ["f1", "m1", "m2", "f2", "m3", "f3", "m4", "m5", "m6", "m7", "f4", "m8", "m9", "f5", "m10", "m11"]
 
-
-for p in data_list_people: 
-    dataframe_1 = data_list_people[p]["training"]
-    dataframe_2 = pd.DataFrame()
-    labels = []
-    id = 1
-    for d in dataframe_1:
-        if len(np.unique(d[hmp.label_tag])) < 2:
-            d["id"] = pd.Series(np.full((1,d.shape[0]), id)[0], index=d.index)
-            d["time"] = pd.Series(range((id-1)*d.shape[0], id*d.shape[0]), index=d.index)
-            labels.append(d["activity"].iloc[0])
-            dataframe_2 = dataframe_2.append(d, ignore_index=True)
-            id = id + 1
-    del dataframe_1  
-    dataframe_3 = dataframe_2.drop(hmp.label_tag, 1)
-    del dataframe_2
-    y = np.array(labels)
-    del labels
-    y2 = pd.Series(y)
-    del y
-
-    y2.index+= 1
-    classes_counts = y2.value_counts()
+for p in persons: 
+    
+    data = hmp.load_training_data_by_people(p)
+    print("Slicing Window....")
+    data_tsfresh, y = hmp.slice_by_window_tsfresh(data, window)
+    y.index += 1
+    del data_tsfresh["activity"]
+    
+    classes_counts = y.value_counts()
     if len(classes_counts) > 1:
-        relevant_features = extract_relevant_features(dataframe_3, y2, column_id='id', column_sort='time')
-        s.save_var(relevant_features, "hmp_relevant_features_fix_window{}relevant_features_{}.pkl".format(slash,p))
-        s.save_var(y2, "hmp_relevant_features_fix_window{}y_{}.pkl".format(slash, p))
+        relevant_features = extract_relevant_features(data_tsfresh, y, column_id='id', column_sort='time')
+        s.save_var(relevant_features, "hmp_relevant_features_best_window{}relevant_features_{}.pkl".format(slash, p))
+        s.save_var(y, "hmp_relevant_features_best_window{}y_{}.pkl".format(slash, p))
+
--- scripts/umafall/umafall_get_all_relevant_features_each_person.py
+++ scripts/umafall/umafall_get_all_relevant_features_each_person.py
@@ -1,12 +1,8 @@
 # -*- coding: utf-8 -*-
-# -*- coding: utf-8 -*-
 # IMPORTS #
 from utils.debug import Debug
-from sklearn.ensemble import ExtraTreesClassifier # Extra Trees
 from models.umafall_model import UMAFALL_Model
 from tsfresh import extract_relevant_features
-import pandas as pd
-import numpy as np
 from pre_processing.processing_db_files import Processing_DB_Files
 from utils.project import Project, slash
 from scripts.save_workspace import save
@@ -16,39 +12,22 @@ Debug.DEBUG = 0
 umafall = UMAFALL_Model()
 processing = Processing_DB_Files()
 project = Project()
-extra_trees = ExtraTreesClassifier(n_estimators = 1000, max_depth=1000, random_state=0) #Good performer
 s = save()
+#window = 10 # Janela Fixa
+window = 30 # Melhor Janela
+persons = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17]
 
-data_list_people = umafall.load_training_data_from_list_people(10, [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17], remove_outliers=0.05, additional_where="and sensor=2 and body=3")# Janela Fixa  # select magnetometer in waist - best accuracy
-#data_list_people = umafall.load_training_data_from_list_people(20, [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17], remove_outliers=0.05, additional_where="and sensor=2 and body=3")# Janela encontrada # select magnetometer in waist - best accuracy
-
-
-for p in data_list_people: 
-    dataframe_1 = data_list_people[p]["training"]
-    dataframe_2 = pd.DataFrame()
-    labels = []
-    id = 1
-    for d in dataframe_1:
-        if len(np.unique(d[umafall.label_tag])) < 2:
-            d["id"] = pd.Series(np.full((1,d.shape[0]), id)[0], index=d.index)
-            d["time"] = pd.Series(range((id-1)*d.shape[0], id*d.shape[0]), index=d.index)
-            labels.append(d["activity"].iloc[0])
-            dataframe_2 = dataframe_2.append(d, ignore_index=True)
-            id = id + 1
-    del dataframe_1  
-    dataframe_3 = dataframe_2.drop(umafall.label_tag, 1)
-    del dataframe_2
-    y = np.array(labels)
-    del labels
-    y2 = pd.Series(y)
-    del y
-
-    y2.index+= 1
-    break
-    classes_counts = y2.value_counts()
+for p in persons: 
+    
+    data = umafall.load_training_data_by_people(p)
+    print("Slicing Window....")
+    data_tsfresh, y = umafall.slice_by_window_tsfresh(data, window)
+    y.index += 1
+    del data_tsfresh["activity"]
+    
+    classes_counts = y.value_counts()
     if len(classes_counts) > 1:
-        relevant_features = extract_relevant_features(dataframe_3, y2, column_id='id', column_sort='time')
-        s.save_var(relevant_features, "umafall_relevant_features_fix_window{}relevant_features_{}.pkl".format(slash, p))
-        s.save_var(y2, "umafall_relevant_features_fix_window{}y_{}.pkl".format(slash, p))
-
+        relevant_features = extract_relevant_features(data_tsfresh, y, column_id='id', column_sort='time')
+        s.save_var(relevant_features, "umafall_relevant_features_best_window{}relevant_features_{}.pkl".format(slash, p))
+        s.save_var(y, "umafall_relevant_features_best_window{}y_{}.pkl".format(slash, p))
 
